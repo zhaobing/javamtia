7fae17b6f11235333b3b145e54c3682e9bfb017d
zhaobing <zhaobing2011@163.com>
2021-01-18 18:46:08 +0800

    增加wait-notify示例

--- JavaMultiThreadInAction/src/io/github/viscent/mtia/ch2/SpeculativeLoadExample.java
+++ JavaMultiThreadInAction/src/io/github/viscent/mtia/ch2/SpeculativeLoadExample.java
@@ -13,31 +13,31 @@ http://www.broadview.com.cn/31065
 package io.github.viscent.mtia.ch2;
 
 public class SpeculativeLoadExample {
-  private boolean ready = false;
-  private int[] data = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+    private boolean ready = false;
+    private int[] data = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
 
-  public void writer() {
-    int[] newData = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
-    for (int i = 0; i < newData.length; i++) {// 语句①（for循环语句）
+    public void writer() {
+        int[] newData = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        for (int i = 0; i < newData.length; i++) {// 语句①（for循环语句）
 
-      // 此处包含读内存的操作
-      newData[i] = newData[i] - i;
+            // 此处包含读内存的操作
+            newData[i] = newData[i] - i;
+        }
+        data = newData;
+        // 此处包含写内存的操作
+        ready = true;// 语句②
     }
-    data = newData;
-    // 此处包含写内存的操作
-    ready = true;// 语句②
-  }
 
-  public int reader() {
-    int sum = 0;
-    int[] snapshot;
-    if (ready) {// 语句③（if语句）
-      snapshot = data;
-      for (int i = 0; i < snapshot.length; i++) {// 语句④（for循环语句）
-        sum += snapshot[i];// 语句⑤
-      }
+    public int reader() {
+        int sum = 0;
+        int[] snapshot;
+        if (ready) {// 语句③（if语句）
+            snapshot = data;
+            for (int i = 0; i < snapshot.length; i++) {// 语句④（for循环语句）
+                sum += snapshot[i];// 语句⑤
+            }
 
+        }
+        return sum;
     }
-    return sum;
-  }
 }
---
+++ JavaMultiThreadInAction/src/prac/ch5/wati_notify1/WaitNotify1.java
@@ -0,0 +1,94 @@
+package prac.ch5.wati_notify1;
+
+import io.github.viscent.mtia.util.Tools;
+
+import java.util.logging.Logger;
+
+
+public class WaitNotify1 {
+
+
+    private final static Logger LOG = Logger.getAnonymousLogger();
+    static Object lock = new Object();
+    static boolean flag = false;
+
+    public static void main(String[] args) {
+        //        new Thread(new WaitThread(),"WaitThread").start();
+        //        Tools.SleepSeconds(1);
+        //        new Thread(new NotifyThread(),"NotifyThread").start();
+
+
+
+        System.out.println("MAIN Start");
+        new Thread(new SleepThread(1),"a").start();
+        new Thread(new SleepThread(5),"b").start();
+        System.out.println("MAIN End");
+
+    }
+
+
+    static class SleepThread implements Runnable {
+        private long sleepSeconds;
+
+        public SleepThread(long sleepSeconds) {
+
+            this.sleepSeconds = sleepSeconds;
+        }
+
+        @Override
+        public void run() {
+            String name = Thread.currentThread().getName();
+            System.out.println(name +" will sleep" + sleepSeconds + " s");
+            Tools.SleepSeconds(sleepSeconds);
+            System.out.println(name +" End");
+        }
+    }
+
+
+    static class WaitThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            //acquire lock
+            synchronized (lock) {
+                while (!flag) {//条件不满足时，则一直等待，等待唤醒线程改变条件，并通知该线程
+                    System.out.println(Thread.currentThread() + " is waiting, flag is " + flag);
+                    try {
+                        //wait()方法会释放锁,当前线程进入等待队列
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }//条件满足
+
+                System.out.println(Thread.currentThread() + " is running, flag is " + flag);
+            }
+        }
+    }
+
+
+    static class NotifyThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            //acquire lock
+            synchronized (lock) {
+                flag = true;
+                //通知wait线程,我已经改变了条件,wait线程可以继续执行了(返回之后继续判断while)
+                //notify操作并不会释放锁，需要等待当前线程释放锁
+                lock.notifyAll();
+                System.out.println(Thread.currentThread() + " hold lock, notify waitThread and flag is " + flag);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + " ready to release lock " + flag);
+            }
+        }
+    }
+
+
+}
